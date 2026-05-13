db5cf10996f4ed38b684f00c051440b8b022197e
aaronbillvicta-commits <aaronbill.victa@gmail.com>
2026-05-12 20:53:00 -0400

    Add files via upload

---
+++ voice-twiml.js
@@ -0,0 +1,17 @@
+module.exports = function handler(req, res) {
+  const to   = (req.body && req.body.To)   || req.query.To   || '';
+  const from = process.env.SW_PHONE_NUMBER || '';
+
+  res.setHeader('Content-Type', 'text/xml');
+
+  if (!to) {
+    return res.end('<?xml version="1.0" encoding="UTF-8"?><Response><Reject/></Response>');
+  }
+
+  res.end(`<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Dial callerId="${from}" timeout="30" record="record-from-ringing">
+    <Number>${to}</Number>
+  </Dial>
+</Response>`);
+};
